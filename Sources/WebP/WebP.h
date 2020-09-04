45cf3c5f2b837e13ab9a3b32920bbb46b27a2880
Marc Rollin <marc@zen.ly>
2020-09-04 11:08:12 +0200

    fix warnings with case sensitive configuration

--- Sources/WebP/WebP.h
+++ Sources/WebP/WebP.h
@@ -6,10 +6,10 @@ FOUNDATION_EXPORT double webpVersionNumber;
 //! Project version string for webp.
 FOUNDATION_EXPORT const unsigned char webpVersionString[];
 
-#import <webp/decode.h>
-#import <webp/demux.h>
-#import <webp/encode.h>
-#import <webp/format_constants.h>
-#import <webp/mux.h>
-#import <webp/mux_types.h>
-#import <webp/types.h>
+#import <WebP/decode.h>
+#import <WebP/demux.h>
+#import <WebP/encode.h>
+#import <WebP/format_constants.h>
+#import <WebP/mux.h>
+#import <WebP/mux_types.h>
+#import <WebP/types.h>
